80af30e9e5f677bf7819c245246009c139a3ed2d
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2021-02-05 11:13:40 -0300

    coinapi

--- src/main/java/com/rbestardpino/App.java
+++ src/main/java/com/rbestardpino/App.java
@@ -1,14 +1,24 @@
 package com.rbestardpino;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class App {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        log.info("App started");
+
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot());
+            botsApi.registerBot(new CryptoTrackerBot());
+
+            log.info("Bot initialized");
+
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
--- src/main/java/com/rbestardpino/CryptoTrackerBot.java
+++ src/main/java/com/rbestardpino/CryptoTrackerBot.java
@@ -1,26 +1,45 @@
 package com.rbestardpino;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Bot extends TelegramLongPollingBot {
+import io.coinapi.rest.Config;
+import io.coinapi.rest.Exchange_rate;
+import io.coinapi.rest.REST_methods;
+
+public class CryptoTrackerBot extends TelegramLongPollingBot {
+
+    private Logger log = LoggerFactory.getLogger(CryptoTrackerBot.class);
+    private REST_methods api;
 
     @Override
     public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
 
             String chatId = String.valueOf(update.getMessage().getChatId());
-            String msg = update.getMessage().getText();
+
+            log.info(update.getMessage().getFrom().getUserName() + ": " + update.getMessage().getText());
 
             SendMessage message = new SendMessage();
             message.setChatId(chatId);
-            message.setText(msg);
+
+            api = new REST_methods("9619CF7F-A991-4149-8527-D8A856BE258F");
 
             try {
-                execute(message); // Call method to send the message
+                Exchange_rate ethusd = api.get_exchange_rate("ETH", "USD");
+                message.setText("ETH/USD: " + ethusd.get_rate() + ". Time: " + ethusd.get_time());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            try {
+                execute(message);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
